c080e3fab8419d88bd2fe71a36ca82fbf08a2fa2
JRframe <1042785075@qq.com>
2025-04-12 13:35:06 +0800

    fix bug in phantom adapter causing session persistence loss on app resume

--- Utils/NodeExtend/TipHelper.ts
+++ Utils/NodeExtend/TipHelper.ts
@@ -1,9 +1,12 @@
 import { tips } from "db://assets/script/game/common/prompt/TipsManager";
+import { oops } from "db://oops-framework/core/Oops";
 
 export default class TipHelper {
-    static showTip() {
+    /** 显示开发中提示 */
+    static showDevTip() {
         tips.confirm("魔盒升级中，尽请期待", () => {
         }, "确定", () => {
         }, "取消", false);
+        oops.gui.waitClose();
     }
 }
