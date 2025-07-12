f129c64eae522ccd56e0f13c2a10d91deb1eb744
1994qin <m18410067595@163.com>
2025-07-12 18:48:58 +0800

    add flutter plugin with dart bindings for all wallet adapters

--- Utils/NodeExtend/TipHelper.ts
+++ Utils/NodeExtend/TipHelper.ts
@@ -1,12 +1,12 @@
-import { TipsNoticeUtil } from "../../../game/gameplay/Utility/TipsNoticeUtil";
+import { tips } from "db://assets/script/game/common/prompt/TipsManager";
+import { oops } from "db://oops-framework/core/Oops";
 
 export default class TipHelper {
     /** 显示开发中提示 */
     static showDevTip() {
-        // tips.confirm("魔盒升级中，尽请期待", () => {
-        // }, "确定", () => {
-        // }, "取消", false);
-        // oops.gui.waitClose();
-        TipsNoticeUtil.PlayNotice("魔盒升级中，敬请期待！");
+        tips.confirm("魔盒升级中，尽请期待", () => {
+        }, "确定", () => {
+        }, "取消", false);
+        oops.gui.waitClose();
     }
 }
