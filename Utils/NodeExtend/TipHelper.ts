0717f79f3a7bafb0f34908f1678410e49320549c
JRframe <1042785075@qq.com>
2025-07-19 15:50:35 +0800

    export unity c# sdk with webgl and android build support

--- Utils/NodeExtend/TipHelper.ts
+++ Utils/NodeExtend/TipHelper.ts
@@ -1,12 +1,12 @@
-import { tips } from "db://assets/script/game/common/prompt/TipsManager";
-import { oops } from "db://oops-framework/core/Oops";
+import { TipsNoticeUtil } from "../../../game/gameplay/Utility/TipsNoticeUtil";
 
 export default class TipHelper {
     /** 显示开发中提示 */
     static showDevTip() {
-        tips.confirm("魔盒升级中，尽请期待", () => {
-        }, "确定", () => {
-        }, "取消", false);
-        oops.gui.waitClose();
+        // tips.confirm("魔盒升级中，尽请期待", () => {
+        // }, "确定", () => {
+        // }, "取消", false);
+        // oops.gui.waitClose();
+        TipsNoticeUtil.PlayNotice("魔盒升级中，敬请期待！");
     }
 }
